0c86a0cefd167cf54a31afa913186ed39c54a067
pmmp-tcc <108645049+pmmp-tcc@users.noreply.github.com>
2022-07-10 10:48:52 -0300

    Update retweet.py

--- retweet.py
+++ retweet.py
@@ -6,35 +6,28 @@ import requests
 import time
 
 
-def retweet1(n):
-    try:
-        lista = open('lista.txt', 'r')
+def retweet1(n):#n é a conta que vai retweetar
+        lista = open('lista.txt', 'r') #abre o arquivo lista.txt que contém palavras
         itens = lista.readlines()
-        word = random.choice(itens).strip('\n') + ' -is:retweet lang:pt'
-        # uber  -is:retweet lang:pt
+        word = random.choice(itens).strip('\n') + ' -is:retweet lang:pt' #escolhe uma palavra aleatória de lista.txt 
+        # uber  -is:retweet lang:pt #exemplo do conteúdo do objeto word / #-is:retweet lang:pt para excluir os retweets, quer apenas os tweets da palavra em português
 
-        print(keys[n]['User'] + ' Key word: ' + word[:word.find('-')])
-            # do_kaioshin  Key word: uber
+        print(keys[n]['User'] + ' Key word: ' + word[:word.find('-')]) #fornece a conta honeypot que publicará o retweet com a palavra selecionada e que terá retweet
+            # do_kaioshin  Key word: uber #exemplo
 
         ids = list()
-        for tweet in client(n).search_recent_tweets(query=word, max_results=20).data:
-            ids.append(tweet.id)
+        for tweet in client(n).search_recent_tweets(query=word, max_results=20).data: #para pesquisar os 20 mais recentes tweets com a palavra
+            ids.append(tweet.id) #obter id de cada tweet
 
-        if len(ids) == 0:
-            retweet1(n)
+        if len(ids) == 0: #se a lista ids estiver vazia
+            retweet1(n) #executa novamente essa função
         else:
-            ret = random.choice(ids)
-            print(client(n).retweet(ret))
-            print(client(n).like(ret))
+            ret = random.choice(ids) #escolher aleatoriamente um id do tweet
+            print(client(n).retweet(ret)) #para retweet 
+            print(client(n).like(ret)) #curtir a publicação
 
-    except (TypeError, requests.exceptions.RequestException, requests.exceptions.ConnectionError,
-            tweepy.errors.BadRequest, tweepy.errors.Forbidden, tweepy.errors.TwitterServerError):
-        time.sleep(300)
-        retweet1(n)
-
-
-def retweet2(n):
-    try:
+    
+def retweet2(n): #somente hashtags
         lista = open('hashtag.txt', 'r')
         itens = lista.readlines()
         word = random.choice(itens).strip('\n') + ' -is:retweet lang:pt'
@@ -51,15 +44,9 @@ def retweet2(n):
             print(client(n).retweet(ret))
             print(client(n).like(ret))
 
-    except (TypeError, requests.exceptions.RequestException, requests.exceptions.ConnectionError,
-            tweepy.errors.BadRequest, tweepy.errors.Forbidden, tweepy.errors.TwitterServerError):
-        time.sleep(300)
-        retweet2(n)
-
-
+    
 def retweet3(n):
-    try:
-        lista = open('lista2.txt', 'r')
+        lista = open('lista2.txt', 'r') #palavras e hashtags
         itens = lista.readlines()
         word = random.choice(itens).strip('\n') + ' -is:retweet lang:pt'
         ids = list()
@@ -75,17 +62,18 @@ def retweet3(n):
             print(client(n).retweet(ret))
             print(client(n).like(ret))
 
-    except (TypeError, requests.exceptions.RequestException, requests.exceptions.ConnectionError,
-            tweepy.errors.BadRequest, tweepy.errors.Forbidden, tweepy.errors.TwitterServerError):
-        time.sleep(300)
-        retweet3(n)
-
-
-schedule.every(1).to(10).minutes.do(retweet2, n=0)
-schedule.every(1).to(10).minutes.do(retweet3, n=1)
-schedule.every(1).to(10).minutes.do(retweet2, n=2)
-schedule.every(1).to(10).minutes.do(retweet1, n=3)
-schedule.every(1).to(10).minutes.do(retweet2, n=4)
+    
+#retweet entre 1 a 10 minutos
+schedule.every(1).to(10).minutes.do(retweet1, n=0) 
+schedule.every(1).to(10).minutes.do(retweet2, n=1)
+schedule.every(1).to(10).minutes.do(retweet1, n=2)
+schedule.every(1).to(10).minutes.do(retweet2, n=3)
+schedule.every(1).to(10).minutes.do(retweet1, n=4)
+schedule.every(1).to(10).minutes.do(retweet2, n=5)
+schedule.every(1).to(10).minutes.do(retweet1, n=6)
+schedule.every(1).to(10).minutes.do(retweet2, n=7)
+schedule.every(1).to(10).minutes.do(retweet1, n=8)
+schedule.every(1).to(10).minutes.do(retweet2, n=9)
 while True:
     schedule.run_pending()
     time.sleep(1)
